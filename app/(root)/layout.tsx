6785e2438270a7462ab3a1b1c3ab89e0ba8d2a14
Charon <charon.s.tom@gmail.com>
2024-03-01 22:23:01 +0100

    setting shadcn clerk mobile and sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
+import Mobilenav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
+      <Sidebar />
+      <Mobilenav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
